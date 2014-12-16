a6c47a9bc2ccb1385c1e354b145121ad15414f16
Aleksandr Bykov <azbykov@yamoney.ru>
2014-12-16 15:53:59 +0300

    rewrite tech use build-flow

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 enb-browserify
-==========
+=========
 
-Обрабатывает файлы browserify (раскрытие require).
+Раскрывает require, используя browserify.
+
+**Опции**
+
+* *String* **source** — Исходный таргет. Обязательная опция.
+* *String* **target** — Результирующий таргет. По умолчанию — `?.browser.js`.
+
+**Пример**
+
+```javascript
+nodeConfig.addTech([ require('enb-browserify/techs/browserify'), {
+source: '?.node.js',
+target: '?.browser.js'
+} ]);
+```
+
--- techs/browserify.js
+++ techs/browserify.js
@@ -1,37 +1,41 @@
-var inherit = require('inherit');
+/**
+ * enb-browserify
+ * =========
+ *
+ * Раскрывает require, используя browserify.
+ *
+ * **Опции**
+ *
+ * * *String* **source** — Исходный таргет. Обязательная опция.
+ * * *String* **target** — Результирующий таргет. По умолчанию — `?.browser.js`.
+ *
+ * **Пример**
+ *
+ * ```javascript
+ * nodeConfig.addTech([ require('enb-browserify/techs/browserify'), {
+ * source: '?.node.js',
+ * target: '?.browser.js'
+ * } ]);
+ * ```
+ */
 var browserify = require('browserify');
-var fs = require('fs');
+var Vow = require('vow');
 
-module.exports = inherit(require('enb/lib/tech/base-tech'), {
-    getName: function () {
-        return 'browserify';
-    },
-
-    configure: function () {
-        this._source = this.getOption('sourceTarget');
-        if (!this._source) {
-            this._source = this.getRequiredOption('source');
-        }
-        this._target = this.getOption('destTarget');
-        if (!this._target) {
-            this._target = this.getRequiredOption('target');
-        }
-    },
-
-    getTargets: function () {
-        return [this.node.unmaskTargetName(this._source)];
-    },
-
-    build: function () {
-        var target = this.node.unmaskTargetName(this._target);
-        var targetPath = this.node.resolvePath(target);
-        var source = this.node.unmaskTargetName(this._source);
-        var sourcePath = this.node.resolvePath(source);
-        var _this = this;
-        browserify(sourcePath).bundle().pipe(
-            fs.createWriteStream(targetPath)
-        ).on('end', function () {
-            _this.node.resolveTarget(target);
+module.exports = require('enb/lib/build-flow').create()
+    .name('browserify')
+    .target('target', '?.browser.js')
+    .defineRequiredOption('target')
+    .defineRequiredOption('source')
+    .useSourceFilename('source')
+    .builder(function (source) {
+        var promise = Vow.promise();
+        browserify(source).bundle(function(err, data) {
+            if (err) {
+                promise.reject(err);
+            } else {
+                promise.fulfill(data);
+            }
         });
-    }
-});
+        return promise;
+    })
+    .createTech();
